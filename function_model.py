fb711da75e42bc267f57396ff12d780eaf24eb84
faskr <franciscoskrobola@gmail.com>
2025-02-07 21:49:21 -0500

    generalized function model

--- function_model.py
+++ function_model.py
@@ -2,19 +2,29 @@ import torch
 import torch.nn as nn
 
 class FunctionModel(nn.Module):
-    def __init__(self, depthH=1, breadth=40, mode='xy'):
+    def __init__(self, depthH=1, breadth=40, mode='xy', x_length=1, y_length=1):
         super(FunctionModel, self).__init__()
         match mode:
             case 'x':
                 self.make_input = self.make_input_x
+                self.y_length = 0
+                inputs = 1
             case 'y':
                 self.make_input = self.make_input_y
+                self.y_length = 1
+                inputs = 1
             case 'xy':
                 self.make_input = self.make_input_xy
+                self.y_length = 1
+                inputs = 2
+            case 'xyy':
+                self.make_input = self.make_input_xy
+                self.y_length = y_length
+                inputs = x_length + y_length
             case _:
                 ValueError("Mode needs to be 'x', 'y', or 'xy'")
         self.depthH = depthH
-        self.linearI = nn.Linear(2, breadth) if mode == 'xy' else nn.Linear(1, breadth)
+        self.linearI = nn.Linear(inputs, breadth)
         self.linearH = nn.Linear(breadth, breadth)
         self.linearO = nn.Linear(breadth, 1)
         self.relu = nn.ReLU()
@@ -39,10 +49,12 @@ class FunctionModel(nn.Module):
     def forward(self, x_interp, y_prev_interp, x_extrap=torch.empty((1, 0))):
         assert x_interp.size(1) == y_prev_interp.size(1)
         # Interpolate
-        output_tensor = self.network(x_interp[:, :, None], y_prev_interp[:, :, None])[:, :, 0]
-        output_list = list(output_tensor.split(1, dim=1))
+        if y_prev_interp.dim() == 2:
+            y_prev_interp = y_prev_interp[:, :, None]
+        output_tensor = self.network(x_interp[:, :, None], y_prev_interp)[:, :, 0]
+        #output_list = list(output_tensor.split(1, dim=1))
         # Extrapolate if there are extrapolation inputs
         for i in range(x_extrap.size(1)):
-            output_list += [self.network(x_extrap[:, i, None], output_list[-1])]
-        output_tensor = torch.cat(output_list, dim=1)
+            output_tensor = torch.cat([output_tensor, self.network(x_extrap[:, i, None], output_tensor[:, -self.y_length:])], dim=1)
+        #output_tensor = torch.cat(output_list, dim=1)
         return output_tensor
